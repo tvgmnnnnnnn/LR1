bf0ef5711caccf1d88dab997938ad1dba13761d4
tvgmnnnnnnn <116604712+tvgmnnnnnnn@users.noreply.github.com>
2022-12-07 03:43:16 +0300

    Add files via upload

---
+++ lr1_opp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr1_opp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTask2Edit test = new MyTask2Edit();
+            test.InputArray();
+            test.PrintResult();
+        }
+    }
+}
